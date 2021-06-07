77547122835258881d574d480c5f97bc63109f83
Felipe González <fgonzalez@miningtag.cl>
2021-06-07 19:52:21 -0400

    added map view model and view

--- RecyApp/RecyApp/AppShell.xaml.cs
+++ RecyApp/RecyApp/AppShell.xaml.cs
@@ -11,8 +11,8 @@ namespace RecyApp
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+            //Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            //Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
 
     }
