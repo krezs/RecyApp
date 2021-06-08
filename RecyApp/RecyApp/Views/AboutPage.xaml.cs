cdfd718854ea93bfade28695c62439910f59086c
Felipe González <fgonzalez@miningtag.cl>
2021-06-07 20:30:46 -0400

    refactor project, map added to map viewmodel and view

--- RecyApp/RecyApp/Views/AboutPage.xaml.cs
+++ RecyApp/RecyApp/Views/AboutPage.xaml.cs
@@ -11,10 +11,6 @@ namespace RecyApp.Views
         public AboutPage()
         {
             InitializeComponent();
-
-            var mapPosition = new Position(-33.436761, -70.656800);
-            var mapSpan = MapSpan.FromCenterAndRadius(mapPosition, Distance.FromMiles(2));
-            MyMap.MoveToRegion(mapSpan);
         }
     }
 }
--- RecyApp/RecyApp/Views/Map.xaml.cs
+++ RecyApp/RecyApp/Views/Map.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
 
 namespace RecyApp.Views
@@ -15,6 +16,35 @@ namespace RecyApp.Views
         public Map()
         {
             InitializeComponent();
+            var mapPosition = new Position(-33.436761, -70.656800); // GEOLOCALIZACION
+            //con la geo. tendremos el punto inicial
+
+
+            var mapSpan = MapSpan.FromCenterAndRadius(mapPosition, Distance.FromKilometers(1));
+            RecyMap.MoveToRegion(mapSpan);
+            
+            //consumir api
+            //pintar puntos a la redonda desde la primera carga
+
+
+            Pin pin = new Pin
+            {
+                Label = "Santa Cruz",
+                Address = "The city with a boardwalk",
+                Type = PinType.Place,
+                Position = mapPosition
+            };
+
+            Pin pin2 = new Pin
+            {
+                Label = "Santa Cruz",
+                Address = "The city with a boardwalk",
+                Type = PinType.Place,
+                Position = new Position(-33.443180, -70.653783)
+            };
+
+            RecyMap.Pins.Add(pin);
+            RecyMap.Pins.Add(pin2);
         }
     }
 }
